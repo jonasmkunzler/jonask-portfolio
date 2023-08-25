9a113599e201b61e7ef9aa8f171193ed4e1ca13d
Jonas Linux Mint <jonasmkunzler@gmail.com>
2023-08-25 17:11:00 -0300

    Enviando ajuste do react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import App from "./App";
-import "./index.css";
-import Project from "./components/Project-One";
+import App from './App'
+import './index.css'
+import Project from './components/Project-One'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    path: '/',
+    element: <App />
   },
   {
-    path: "taskmanager",
-    element: <Project />,
-  },
-]);
+    path: '/taskmanager',
+    element: <Project />
+  }
+])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
