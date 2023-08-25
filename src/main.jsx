99ecb24e07944b35f1cdc5d886487825d03af30f
Jonas Linux Mint <jonasmkunzler@gmail.com>
2023-08-25 17:34:51 -0300

    Enviando novos ajustes para tentar corrigir erro do react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
-import Project from './components/Project-One'
+import Project from './components/ProjectOne'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: (
+      <>
+        <App />
+      </>
+    )
   },
   {
     path: '/taskmanager',
-    element: <Project />
+    element: (
+      <>
+        <Project />
+      </>
+    )
   }
 ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
