19b4dba2be164228315e1441ff35b6a3037331d1
Jonas Linux Mint <jonasmkunzler@gmail.com>
2023-11-29 15:04:24 -0300

    Enviando alteração na pagina principal

--- src/main.jsx
+++ src/main.jsx
@@ -27,7 +27,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <img src={thumbnailImage} alt="Thumbnail" />
-    <RouterProvider router={router} />
+    <RouterProvider router={router}>
+      <img src={thumbnailImage} alt="Thumbnail" />
+    </RouterProvider>
   </React.StrictMode>
 )
