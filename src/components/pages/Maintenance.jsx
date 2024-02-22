0f0370751fa56d56203054ee44088ea719c01cc1
JonasUbuntu <jonasmkunzler@gmail.com>
2024-02-22 15:30:35 -0300

    mansagement

--- src/components/pages/Maintenance.jsx
+++ src/components/pages/Maintenance.jsx
@@ -15,7 +15,7 @@ export function Maintenance() {
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        Under Maintenance...
+        Under <br /> Maintenance
       </motion.h1>
     </LampContainer>
   );
